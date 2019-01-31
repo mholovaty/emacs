40c11bf7b6fa433515a22180abb65d33979e65e0
Mykhailo Golovatyi <mgolovatyi@flowtraders.com>
2019-01-31 23:12:32 +0100

    Update elpa modules

---
+++ elpa/cmake-project-0.7/cmake-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-project" "0.7" "Integrates CMake build process with Emacs" 'nil :commit "ec61f687772cccdb699f64ebe1e8dc8ba83f790f" :keywords '("c" "cmake" "languages" "tools") :authors '(("Alexander Lamaison" . "alexander.lamaison@gmail")) :maintainer '("Alexander Lamaison" . "alexander.lamaison@gmail") :url "http://github.com/alamaison/emacs-cmake-project")
---
+++ elpa/fill-column-indicator-1.87/fill-column-indicator-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fill-column-indicator" "1.87" "Graphically indicate the fill column" 'nil :commit "0e755319451dd9c6c99c2a2ef82c890ba93343b6" :keywords '("convenience"))
